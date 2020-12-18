56f723218cb69d2e5c8593583dc5e204744b5ca1
Bülent ALA <bulentala@gmail.com>
2020-12-18 14:18:20 +0300

    env test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Index = () => {
-  return <div>Play with Next.Js</div>;
+const Index = ({ posts }) => {
+  return <div>{posts.name}</div>;
 };
-
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.BASE_API_URL}`);
+  const posts = await res.json();
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 export default Index;
