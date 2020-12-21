78a2d438bde8492ee1bc04bb4fb61531fbecd6e7
Bülent Ala <bulent@dctfethiye.local>
2020-12-21 16:05:00 +0300

    test

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import Head from "next/head";
+import MenuIcon from "components/menuIcon";
 
-const Index = ({ data }) => {
-  return <div>{data.name}</div>;
+const Index = () => {
+  return (
+    <>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css'
+          integrity='sha512-+WF6UMXHki/uCy0vATJzyA9EmAcohIQuwpNz0qEO+5UeE5ibPejMRdFuARSrl1trs3skqie0rY/gNiolfaef5w=='
+          crossorigin='anonymous'
+        />
+      </Head>
+    </>
+  );
 };
-export async function getStaticProps() {
-  const res = await fetch(`${process.env.API_BASE_URL}en`);
-  const data = await res.json();
-  return {
-    props: {
-      data,
-    },
-  };
-}
+export const config = { amp: true };
 export default Index;
